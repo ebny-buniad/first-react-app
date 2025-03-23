0e60b26336cfd53ff94fa7d361bf3380e7174d40
ebnybuniad <ebnybuniad@hmail.com>
2025-03-23 14:07:15 +0600

    added a

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + Hello React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count is a {count}
         </button>
       </div>
     </>
